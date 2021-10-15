c5b95357b7958f0d050346313f586f8f353b890e
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-10-15 03:52:39 -0300

    archivo php modificado para validar y enviar form

--- correo.php
+++ correo.php
@@ -1,17 +1,59 @@
 <?php
-  $destinatario = 'fernando-ramos94@hotmail.com';
-  $nombre=$_POST['name_lastname'];
-  $email=$_POST['email'];
-  $telefono=$_POST['phone'];
-  $mensaje=$_POST['Consulta'];
+  $destinatario = 'flippededu@hotmail.com';
+  $nombreErr = $emailErr = $telefonoErr = "" ;
+  $nombre = $email = $telefono=  $mensaje= "" ;
 
-  $header = "Enviado desde la pagina de FLIPPED";
-  $mensajeCompleto = "Nombre y apellido: " .$nombre "\n"
-                      "email: ".$email "\n";
-                      "telefono: ".$telefono "\n"
-                      "Asunto: ".$mensaje "\n";
 
-  mail($destinatario,$mensajeCompleto,$header);
-  echo "<script>alert('Correo enviado exitosamente')</script>";
-  echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+  function validar_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //Verificar y validar nombre
+    if($_POST['name_lastname'] == preg_replace('([^A-Za-z])', ' ', $_POST['name_lastname'])){
+      $nombre = validar_input($_POST['name_lastname']);
+    }
+    else{
+      $nombreErr = "Por favor, no utilice numeros ni caracteres especiales en tu nombre";
+    } 
+
+    //Verificar y validar 
+    
+    $email = validar_input($_POST['email']); 
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $emailErr = "Formato de email invalido"; 
+    }
+
+
+    //Verificar y validar telefono
+    if($_POST['phone'] == preg_replace('([^0-9-+])','', $_POST['phone'])){
+      $telefono = $_POST['phone'];
+    }
+    else{
+      $telefonoErr = "Por favor, solo coloque numeros en este campo";
+    }
+
+
+    //Verificar y validar nombre
+    $mensaje = validar_input($_POST['Consulta']);
+    
+
+    $asunto="Consulta";
+    $header = "Enviado desde flippededu-online";
+    $mensajeCompleto = "Nombre completo: " . $nombre ."\n\nEmail: ".$email. "\n\nTelefono: ".$telefono. "\n\nAsunto: ".$mensaje. "\n";
+  
+    if (empty($nombreErr) and empty($emailErr) and empty($telefonoErr)) {
+      mail($destinatario,$asunto,$mensajeCompleto,$header);
+      echo "<script>alert('Correo enviado exitosamente')</script>";
+      echo "<script>setTimeout(\"location.href='Contacto.html'\",1000)</script>";
+    }
+    else{
+      echo "<script>alert('Correo no enviado')</script>";
+      echo "<script>setTimeout(\"location.href='Contacto.html'\",1000)</script>";
+    }
+  } 
 ?>
