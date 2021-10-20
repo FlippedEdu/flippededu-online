b8b4d271124cc17026682ce6cacb780edfb6bf1f
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-10-19 22:59:55 -0300

    arreglos de estilos del formulario

--- js/formEvent.js
+++ js/formEvent.js
@@ -1,12 +1,28 @@
-let divAzulVacio = document.getElementsByClassName("containerVacioAzul");
-let miFormulario = document.getElementById("contactform123");
+/*let divAzulVacio = document.getElementsByClassName("containerVacioAzul");
+
+let miFormulario = document.getElementById("form");
 
 
 const validarFormulario= (e)=>{
   e.preventDefault();
+  divAzulVacio.style.top = "0";
   divAzulVacio.style.height = "1000px";
+  divAzulVacio.style.background = "green";
 }
 
 
-miFormulario.addEventListener("submit",validarFormulario);
+miFormulario.addEventListener("submit",validarFormulario);*/
+
+
+  const iframe = document.getElementById("contactform123");
+  var elmnt = iframe.contentWindow.document.getElementById("form");
+
+  function algo (){
+    console.log("logrado");
+  }
+
+elmnt.addEventListener("submit",algo);
+
+
+
 
